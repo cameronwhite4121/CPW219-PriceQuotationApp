79c61ea6a7165c12cb04631d3401bf089e4b8a09
Cameron White <cameronwhite4121@gmail.com>
2024-08-08 15:41:00 -0700

    Added properties to be accessed by the Quote view

--- CPW219-PriceQuotationApp/Models/Price.cs
+++ CPW219-PriceQuotationApp/Models/Price.cs
@@ -1,25 +1,44 @@
-﻿namespace CPW219_PriceQuotationApp.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_PriceQuotationApp.Models
 {
     public class Price
     {
         /// <summary>
         /// Price before any discounts are applied
         /// </summary>
-        decimal Subtotal { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Subtotal { get; set; }
 
         /// <summary>
-        /// Percentage of subtotal that is discounted
+        /// Percentage of subtotal that is discounted (0-100)
         /// </summary>
-        decimal DiscountPercent { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int DiscountPercent { get; set; }
 
         /// <summary>
         /// Price that was subtracted from subtotal
-        /// </summary>
-        decimal DiscountAmount { get; set; }
+        /// </summary
+        public decimal DiscountAmount 
+        {
+            get 
+            {
+                return (Decimal)Subtotal * DiscountPercent / 100;
+            }
+        }
 
         /// <summary>
         /// Price after discounts are applied
         /// </summary>
-        decimal Total { get; set; }
+        public decimal Total 
+        {
+            get 
+            {
+                return (Decimal)Subtotal - DiscountAmount;
+            } 
+        }
     }
 }
