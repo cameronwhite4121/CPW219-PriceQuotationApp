20f78388726ee47f2a379daa7645ed8aaa98ded6
Cameron White <cameronwhite4121@gmail.com>
2024-08-08 15:15:37 -0700

    Problems with merging, please merge

---
+++ CPW219-PriceQuotationApp/Models/Price.cs
@@ -0,0 +1,25 @@
+﻿namespace CPW219_PriceQuotationApp.Models
+{
+    public class Price
+    {
+        /// <summary>
+        /// Price before any discounts are applied
+        /// </summary>
+        decimal Subtotal { get; set; }
+
+        /// <summary>
+        /// Percentage of subtotal that is discounted
+        /// </summary>
+        decimal DiscountPercent { get; set; }
+
+        /// <summary>
+        /// Price that was subtracted from subtotal
+        /// </summary>
+        decimal DiscountAmount { get; set; }
+
+        /// <summary>
+        /// Price after discounts are applied
+        /// </summary>
+        decimal Total { get; set; }
+    }
+}
